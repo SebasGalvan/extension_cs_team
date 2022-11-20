f1adec6eba11874ef374861299a6f139dff0087f
Sebastián Galván <sebastiangalvan.ar@gmail.com>
2022-11-19 23:49:18 -0300

    [UPDATE] Axios por fetch

--- js/back.js
+++ js/back.js
@@ -1,29 +1,38 @@
-const inputPassword = document.querySelector(".password")
-const btnSubmit = document.querySelector(".enviar")
-const contenedor_alg = document.querySelectorAll(".algoritmos_col")
+document.querySelector("#enviar").addEventListener("click", getPasswords)
 
-btnSubmit.addEventListener("submit", getPasswords);
+const inputPassword = document.querySelector("#password")
 
-async function getPasswords(e){
-    e.preventDeffault()
-    let password = inputPassword.value;
-    try{
-        const req =  await axios({
-            method: 'post',
-            url: 'https://team-pro-utn.onrender.com/api/v1/password',
-            data: {
-              password: password,
-            }
-          });
 
-          await req.forEach(element => {
-            let elem = document.createElement("div")
-            elem.innerHTML = element
-            contenedor_alg.appendChild(elem);
-          });
+function getPasswords(){
+    const data = { password: inputPassword.value };
 
-          
-    }catch(error){
-        console.log("PASO ALGO");
-    }
+  fetch('https://team-pro-utn.onrender.com/api/v1/password', {
+  method: "POST",
+  body: JSON.stringify(data),
+  headers: {"Content-type": "application/json; charset=UTF-8"}
+})
+  .then(response => response.json()) 
+  .then(json => insertarHash(json))
+  .catch(err => console.log(err));
+}
+
+
+function insertarHash(res){
+
+    const contenedor_alg = document.querySelector(".container_algo")
+
+    for (let hash in res) {
+        const elem = document.createElement("div")
+        elem.className = "algo_labels"
+
+        const algoritmo = document.createElement("span")
+        algoritmo.innerHTML = hash+ ": "
+        const algoritmo_hash = document.createElement("span")
+        algoritmo_hash.innerHTML = res[hash]
+
+        elem.appendChild(algoritmo)
+        elem.appendChild(algoritmo_hash)
+
+        contenedor_alg.appendChild(elem);
+      }
 }
